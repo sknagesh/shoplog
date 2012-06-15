db9e6e55ec0336fa42488349d096e438d5d6c03b
S K Nagesh <sknagesh@gmail.com>
2012-06-15 10:28:47 +0530

    bug fix

--- get_latest_end_time.php
+++ get_latest_end_time.php
@@ -6,7 +6,7 @@ $mid=$_GET['mid'];
 //print($mid);
 
 
-$query="SELECT DATE_FORMAT(End_Date_Time,'%d/%m/%Y %h:%i %p')as edt FROM ActivityLog WHERE Machine_ID='$mid' AND Activity_ID IN(1,2,3,4,5,6,8) ORDER BY End_Date_Time DESC LIMIT 1;";
+$query="SELECT DATE_FORMAT(End_Date_Time,'%d/%m/%Y %h:%i %p')as edt FROM ActivityLog WHERE Machine_ID='$mid' AND Activity_ID IN(1,2,3,4,5,6,8,7) ORDER BY End_Date_Time DESC LIMIT 1;";
 
 $resa = mysql_query($query, $cxn) or die(mysql_error($cxn));
 while ($row = mysql_fetch_assoc($resa))
